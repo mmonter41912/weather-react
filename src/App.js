fb21c0cd51bfc93038f2ef70f224eecdaf8f741c
mmonter41912 <montermarlen@gmail.com>
2020-12-14 18:37:53 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import Weather from "./Weather"
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by <a href="https://www.marlenalvarado.com" target="_blank">Marlen Alvarado</a> and is {""}
-        <a 
-        href="https://github.com/mmonter41912/weather-react"
-        target="_blank"
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          This project was coded by <a href="https://www.marlenalvarado.com" target="_blank">Marlen Alvarado</a> and is {""}
+          <a 
+          href="https://github.com/mmonter41912/weather-react"
+          target="_blank"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
+      
     </div>
   )
 }
