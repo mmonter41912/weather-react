94fdffb1b274be80ccd6913c30fa4a9b3e062085
mmonter41912 <montermarlen@gmail.com>
2020-12-15 00:32:44 -0800

    added HTML and CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
-import Weather from "./Weather"
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <Weather />
         <footer>
           This project was coded by <a href="https://www.marlenalvarado.com" target="_blank">Marlen Alvarado</a> and is {""}
           <a 
--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,45 @@
 import React from "react";
 import axios from "axios";
+import WeatherTemperature from "./WeatherTemperature";
+import "./Weather.css";
 
-export default function Weather (props){
-    function handleResponse(response){
-        alert(
-            `The weather in ${response.data.name} is ${response.data.main.temp}°C`
-        );
-    }
-    let apiKey = "f7bcaae56975747b83d88801e136e5f9";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+export default function Weather (){
+    return(
+        <div className="Weather">
+            
+            <div className="row">
+                <nav className="navbar navbar-transparent" className="col">
+                    <form className="form-inline" id="search-form">
+                        <input className="form-control mr-sm-2" type="search" id="city-input" placeholder="Search" aria-label="Search" />
+                        <button className="btn">
+                            <i className="fa fa-search"></i></button>
+                        <button className="btn btn-succes" id="currentLocationButton">
+                            <i className="fas fa-location-arrow"></i></button>
+                    </form>
+                </nav>
+                
+            </div>
+            <div className="row">
+             <h1 className="col" id="city">
+                 San Francisco
 
+             </h1>
+            </div>
+
+            <div className="row">
+                <div className="col">
+                    
+                    <h2 className="realDateTime">Monday 09:33</h2>
+                    <small className="description">Clear</small>
+                  
+                </div>
+            </div>
+
+            <WeatherTemperature />
+
+
+        </div>
+    )
     
     
     
