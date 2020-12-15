287e6942f912649a7368546a6aff7cb4615356e0
mmonter41912 <montermarlen@gmail.com>
2020-12-14 18:02:02 -0800

    Added GitHub url

--- src/App.js
+++ src/App.js
@@ -3,5 +3,17 @@ import './App.css';
 import Weather from "./Weather"
 
 export default function App() {
-  return <div className="App">TEST</div>;
+  return (
+    <div className="App">
+      <footer>
+        This project was coded by <a href="https://www.marlenalvarado.com">Marlen Alvarado</a> and is {""}
+        <a 
+        href="https://github.com/mmonter41912/weather-react"
+        target="_blank"
+        >
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  )
 }
